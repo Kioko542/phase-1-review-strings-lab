eede2b1c1ebb112d3520d29cabd200d144c3df0b
Emmanue <kioxmanu7@gmail.com>
2023-09-18 12:41:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const  currentUser="Manwel";
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+console.log(welcomeMessage)
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting=`"Welcome, ":, ${currentUser.slice(0,1)}!`
